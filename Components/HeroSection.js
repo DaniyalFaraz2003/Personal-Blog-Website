5d4f263cc9ebffdc4f0d6aadb2bde7c6a1639cc3
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-10-20 00:13:28 +0500

    hero section changes

--- Components/HeroSection.js
+++ Components/HeroSection.js
@@ -1,31 +1,57 @@
-export default function HeroSection() {
+"use client";
+
+import TypingAnimator from 'react-typing-animator';
+import { Button } from "@/components/ui/button";
+
+export default function HeroSectionWithEmailInput() {
+    const textArray = ["Software Engineer", "Full-Stack Developer", "Team Lead"]
     return (
         <>
             {/* Hero */}
-            <div className="relative overflow-hidden py-24 lg:py-32">
-                <div className="container">
-                    <div className="max-w-2xl text-center mx-auto">
-                        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                            Nano-Frontiers: Reshaping Tech
-                        </h1>
+            <div className="container">
+                {/* Grid */}
+                <div className="grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-center">
+                    <div className="lg:col-span-3">
                         <p className="mt-3 text-xl text-muted-foreground">
-                            Nano-computing breaks barriers, unlocking new tech horizons.
+                            Hello I am,
                         </p>
+                        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                            Daniyal Faraz
+                        </h1>
+                        <TypingAnimator
+                            textArray={textArray}
+                            cursorColor="#333"
+                            textColor="#555"
+                            fontSize="24px"
+                            loop
+                            typingSpeed={60}
+                            delaySpeed={1000}
+                            backspace
+                            height="60px"
+                            dynamicDelay
+                            style={{fontFamily: "Helvetica" , fontWeight: "bold", marginTop: "10px"}}
+                        />
+
+                        {/* Brands */}
+                        <div className="mt-6 lg:mt-12">
+                            <span className="text-xs font-medium uppercase">my toolbox:</span>
+                            <div className="mt-4 flex gap-x-8">
+                                <img alt="Static Badge" src="https://img.shields.io/badge/with%20a%20logo-grey?style=for-the-badge&logo=javascript"/>
+                            </div>
+                        </div>
+                        {/* End Brands */}
                     </div>
-                    <div className="mt-10 relative max-w-5xl mx-auto">
+                    {/* End Col */}
+                    <div className="lg:col-span-4 mt-10 lg:mt-0">
                         <img
-                            src="https://placehold.co/1024x480"
-                            className="rounded-xl"
+                            className="w-full rounded-xl"
+                            src="https://placehold.co/700x540"
                             alt="Image Description"
                         />
-                        <div className="absolute bottom-12 -start-20 -z-[1] w-48 h-48 bg-gradient-to-b from-primary-foreground via-primary-foreground to-background p-px rounded-lg">
-                            <div className="w-48 h-48 rounded-lg bg-background/10" />
-                        </div>
-                        <div className="absolute -top-12 -end-20 -z-[1] w-48 h-48 bg-gradient-to-t from-primary-foreground via-primary-foreground to-background p-px rounded-full">
-                            <div className="w-48 h-48 rounded-full bg-background/10" />
-                        </div>
                     </div>
+                    {/* End Col */}
                 </div>
+                {/* End Grid */}
             </div>
             {/* End Hero */}
         </>
