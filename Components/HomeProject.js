91192a60e9aaffe8e4443ee933c2e38626b293cc
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-11-05 21:38:36 +0500

    project card complete

--- Components/HomeProject.js
+++ Components/HomeProject.js
@@ -12,21 +12,23 @@ import {
 } from "@/components/ui/card"
 
 import project1 from "../assets/Project1.jpeg"
+import project2 from "../assets/Project2.png"
+import ghIcon from "../assets/gh-icon.png"
 
-function ProjectCard() {
+function ProjectCard({img, title, description}) {
     return (
         <>
             <Card className="w-[350px]">
                 <CardHeader>
-                    <Image src={project1} alt="Project" />
+                    <Image src={img} alt="Project" className={"w-full h-48"} />
                 </CardHeader>
                 <CardContent className={"flex flex-col gap-3"}>
-                    <CardTitle className={"font-bold text-lg"}>Inventory Management System</CardTitle>
-                    <CardDescription className={"text-md"}>An inventory management system designed to streamline items handling processes and automate inventory mangement tasks</CardDescription>
+                    <CardTitle className={"font-bold text-lg"}>{title}</CardTitle>
+                    <CardDescription className={"text-md"}>{description}</CardDescription>
                 </CardContent>
-                <CardFooter className="flex items-center justify-between">
-                    <Button variant="outline">Details</Button>
-                    <Button>Deploy</Button>
+                <CardFooter className="flex justify-between">
+                        <Button variant="outline">Details</Button>
+                        <Button className={"rounded-full w-fit p-0 h-fit"}><Image className={""} src={ghIcon} alt={"github"} /></Button>
                 </CardFooter>
             </Card>
         </>
@@ -34,6 +36,19 @@ function ProjectCard() {
 }
 
 export default function HomeProject() {
+    const projects = [
+        {
+            img: project1,
+            title: "Inventory Management System",
+            description: "An inventory management system designed to streamline items handling processes and automate inventory management tasks"
+        },
+        {
+            img: project2,
+            title: "Gym Management System",
+            description: "A centralized system to handle members and trainers, along with their workout and diet plans, memberships and gym branches over a wide network"
+        }
+
+    ]
     return (
         <>
             <section className='z-0 flex flex-col mb-10 mt-10'>
@@ -42,7 +57,11 @@ export default function HomeProject() {
                     <p className={"font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]"}>My Projects</p>
                     <p className={"text-muted-foreground md:text-[20px] sm:text-[13px] xs:text-[10px] text-[10px]"}>That bring my ideas to life, showcasing my skills and creativity.</p>
                 </div>
-                <ProjectCard />
+                <div className={"flex flex-row gap-3 w-full justify-center"}>
+                    {projects.map((project, index) => {
+                        return <ProjectCard {...project} key={index} />
+                    })}
+                </div>
             </section>
         </>
     )
