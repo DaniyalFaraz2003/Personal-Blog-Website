99b83de4c8749906f4e960332010d214600fa0af
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-12-24 23:05:16 +0500

    Portfolio part under dev

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import {ContentLayout} from "@/Components/admin-panel/content-layout";
+import Image from 'next/image'
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/Components/ui/card";
+import project1 from '../../assets/Project1.jpeg'
+import project2 from '../../assets/Project2.png'
 
 function HeroSection() {
     return (
@@ -10,10 +14,54 @@ function HeroSection() {
     )
 }
 
+function ProjectCard({ title, description, image }) {
+    return (
+        <div className="flex flex-row items-center w-full p-4 border rounded-lg shadow-lg">
+            <CardContent className="basis-[3/5]">
+                <CardTitle className="font-extrabold text-3xl mb-2">{title}</CardTitle>
+                <CardDescription className="text-xl">{description}</CardDescription>
+            </CardContent>
+            <Image
+                src={image}
+                alt="project"
+                className="w-64 h-44 object-contain flex-shrink-0 ml-auto"
+            />
+        </div>
+    );
+}
+
+
+
+function Projects() {
+    const projectData = [
+        {
+            title: 'Inventory Management System',
+            description: 'An inventory management system that helps businesses keep track of their inventory.',
+            image: project1
+        },
+        {
+            title: 'Gym Management System',
+            description: 'A gym management system that helps gym owners keep track of their members and their subscriptions.',
+            image: project2
+        },
+    ]
+
+    return (
+        <section>
+            <div className={'flex flex-col gap-5 my-10'}>
+                {projectData.map((project, index) => {
+                    return <ProjectCard key={index} {...project} />
+                })}
+            </div>
+        </section>
+    )
+}
+
 export default function Portfolio() {
     return (
         <ContentLayout pathname="Portfolio">
             <HeroSection />
+            <Projects />
         </ContentLayout>
     )
 }
