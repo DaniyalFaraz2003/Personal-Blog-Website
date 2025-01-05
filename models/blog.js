f43de6205f6174675ff3b6e9cf2fd622a8b23252
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2025-01-05 11:17:56 +0500

    models defined for the project and blogs

---
+++ models/blog.js
@@ -0,0 +1,11 @@
+const blogPost = {
+    id: "",
+    title: "",
+    username: "",
+    date: "",
+    description: "",
+    badges: ["", "", ""],
+    image: "", // path of the image on the file system
+    latest: true,
+    content: ""
+}
---
+++ models/project.js
@@ -0,0 +1,26 @@
+const projectData = {
+    id: "",
+    title: "",
+    description: "",
+    image: "", // path of the image on the file system
+    githubUrl: "",
+    demoUrl: "",
+    copyRightInfo: "",
+    problem: "",
+    solution: "",
+    techStack: ["", "", ""],
+    features: [
+        {
+            title: "",
+            description: ""
+        },
+        {
+            title: "",
+            description: ""
+        },
+        {
+            title: "",
+            description: ""
+        }
+    ]
+}
